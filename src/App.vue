a87672e130d9775e4ceadf90296392cb1b4d1a07
husainmodiwala <husain.modiwala@nagarro.com>
2023-10-03 15:12:50 +0530

    testing

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+   <!-- <HomePage/> -->
+   <router-view></router-view>
+</template>
+
+<script>
+
+// import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+}
+}
+</script>
+
+<style>
+</style>
